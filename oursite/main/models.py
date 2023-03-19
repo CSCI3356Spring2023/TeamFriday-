50403189a0309400e2b0d9d9e46309635d0da9a6
Abdu <hoseina@bc.edu>
2023-03-19 15:48:44 -0400

    Comments

--- oursite/main/models.py
+++ oursite/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+## Creating custom user class
+# Have three boolean fields, is_student, is_instructor, is_admin
+# Class User(AbstractUser):
+#.....
+
+
+# Student profile model
+# all info related to a student from d3 pdf.
+# Class Student(models.model):
+# .......
+#  ....
+# ....
