7e22c2c5766169797854d35908785151a89994c4
AbduHosein <hoseina@bc.edu>
2023-03-20 22:42:29 -0400

    login decorator

--- oursite/main/views.py
+++ oursite/main/views.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login/')
 def home(response):
     return render(response, 'main/home.html', {})
 
+def fp(response):
+    return render(response, 'main/frontpage.html', {})
 # StudentSignupView
 
 # CreateCourseView
