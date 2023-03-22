e86e73db4c6730449582c340ce172b8bc79425bc
AbduHosein <hoseina@bc.edu>
2023-03-21 21:22:13 -0400

    Updated login redirect

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/home"
 
 LOGOUT_REDIRECT_URL = "/login"
 
