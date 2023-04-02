75e49bdda650a0b16cf55798157761f421d19266
Abdu <hoseina@bc.edu>
2023-04-02 14:44:13 -0400

    Reset back to: Updated course summary url

--- oursite/main/urls.py
+++ oursite/main/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("home/", views.home, name="home"),
     path("", views.fp, name="front page"),
-    path("home/",views.CourseListView.as_view(),name="home page"),
+    # path("/addCourse",views.addCourse,name="addCourse"),
 ]
