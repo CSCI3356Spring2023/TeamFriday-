5b075c4982f8dac03cd3c1986144a55f6952605f
Jonathan Qian <jq@Jonathans-MacBook-Air-4.local>
2023-04-04 07:39:12 -0400

    settings.py email

--- oursite/oursite/settings.py
+++ oursite/oursite/settings.py
@@ -142,3 +142,15 @@ LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/login/"
 
 AUTH_USER_MODEL = 'main.User'
+
+
+
+# Email creation/settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'BCEagleHire@gmail.com'
+EMAIL_HOST_PASSWORD = 'PlsTUwRIKApuVaMo2871'
+
+
