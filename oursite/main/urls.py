a1479e70fce280887c59c3b111e376c406ce66f7
AbduHosein <hoseina@bc.edu>
2023-04-04 00:48:14 -0400

    Course summary login decorator

--- oursite/main/urls.py
+++ oursite/main/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     path("", views.fp, name="front page"),
-    path("home/",views.CourseListView.as_view(),name="home page"),
+    path("home/",login_required(views.CourseListView.as_view()),name="home page"),
     path("apply/<int:id>",views.apply, name="apply"),
     path("instructor_summary/",views.InstructorSummaryView, name="instructor_summary"),
 ]
